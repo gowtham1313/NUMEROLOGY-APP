43d56e214fad5ac03679d17a99f8ecef7ba27cc7
gowtham1313 <116960725+gowtham1313@users.noreply.github.com>
2026-01-01 22:31:46 +0530

    Add files via upload

---
+++ numerology-angular-ui/Src/App/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h2>Numerology Calculator</h2>
+
+    <input [(ngModel)]="name" placeholder="Enter name" />
+    <button (click)="calculate()">Calculate</button>
+
+    <p *ngIf="result">Result: {{ result }}</p>
+  `
+})
+export class AppComponent {
+  name = '';
+  result: number | null = null;
+
+  API = 'https://numerology-api.onrender.com';
+
+  constructor(private http: HttpClient) {}
+
+  calculate() {
+    this.http.post<any>(`${this.API}/numerology`, { name: this.name })
+      .subscribe(res => this.result = res.number);
+  }
+}
